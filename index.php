94b761b73e7c4cac558447d80490d8494f497ded
jonathancaron <jonathan.caron04@gmail.com>
2017-12-05 11:45:38 +0100

    Création du formulaire html

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>Ajax Chat</title>
+  </head>
+  <body>
+    <h1>Ajax Chat</h1>
+    <form class="formulaire" method="post">
+      <input class="nom" type="text" placeholder="Nom"><br>
+      <textarea class="message" rows="6" cols="60" placeholder="Message"></textarea><br>
+      <button type="button">Envoyer</button>
+    </form>
+  </body>
+</html>
